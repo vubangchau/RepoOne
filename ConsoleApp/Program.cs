0e53defc82353c2e03ed8ea17f6d8e487c3a7ead
Derrick Nguyen <ptkn2001@msn.com>
2021-08-23 23:54:37 -0700

    add a new console app

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass2 mc = new MyClass2("it's me");
+            Console.WriteLine(mc.MyProperty);
+            Console.WriteLine(mc.MyMethod());
+        }
+    }
+
+    public interface IMyClass : IEnumerable
+    {
+        string MyMethod();
+        string MyProperty { get; }
+    }
+
+
+    public class MyClass2 : IMyClass
+    {
+        private string myProperty = string.Empty;
+        public MyClass2(string myProperty)
+        {
+            this.myProperty = myProperty;
+        }
+        public string MyProperty { get => this.myProperty; }
+
+        public IEnumerator GetEnumerator() => throw new NotImplementedException();
+
+        public string MyMethod() => "Hello!";
+    }
+
+}
